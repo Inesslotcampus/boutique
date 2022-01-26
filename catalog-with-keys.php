2175c04b4aac8287e12437754e30a252ce760392
Inès <ines.slot@gmail.com>
2022-01-26 09:54:35 +0100

    ajout du catalogue avec clés

---
+++ catalog-with-keys.php
@@ -0,0 +1,47 @@
+<?php
+$iphone = [
+    "name" => "iPhone",
+    "price" => 45000,
+    "weight" => 200,
+    "discount" => 10,
+    "picture_url" => "<img src=https://th.bing.com/th/id/OIP.ur1Px9DpDd4qcIx00iicYwHaKX?w=133&h=186&c=7&r=0&o=5&pid=1.7>",
+    ];
+    echo "<pre>";
+    print_r($iphone);
+    echo "</pre>";
+    //print_r($iphone["price"]);
+    // echo "<br>";
+    // print_r($iphone["weight"]);
+    // echo "<br>";
+    // print_r($iphone["discount"]);
+    // echo "<br>";
+    // print_r($iphone["picture_url"]);
+    // echo "<br>";
+
+
+$ipad = [
+    "name" => "ipad",
+    "price" => 83900,
+    "weight" => 500,
+    "discount" => 10,
+    "picture_url" => "<img src=https://m.media-amazon.com/images/I/81+N4PFF7jS._AC_SX466_.jpg>",
+    ];
+    echo "<pre>";
+    print_r($ipad);
+    echo "</pre>";
+
+   
+    $iMac = [
+        "name" => "iMac",
+        "price" => 224900,
+        "weight" => 1200,
+        "discount" => 10,
+        "picture_url" => "<img src=https://www.apple.com/newsroom/images/product/imac/standard/apple_new-imac-spring21_hero_04202021.jpg.og.jpg?202112030956>",
+        ];
+
+        echo "<pre>";
+        print_r($iMac);
+        echo "</pre>";
+
+       
+    
--- simple-catalog.php
+++ simple-catalog.php
@@ -1,6 +1,7 @@
 <?php
 $products = ["iPhone", "iPad", "iMac"];
 sort($products);
+print_r($products);
   print_r($products[0]);
   echo "<br>";
   print_r(  $products[2]);
