c8b806e465259459de26f67411417ff172317a0b
Inès <ines.slot@gmail.com>
2022-02-01 10:37:53 +0100

    rajout des boucles

--- simple-catalog.php
+++ simple-catalog.php
@@ -3,8 +3,38 @@
 
 include "my-function.php";
 $products = ["iPhone", "iPad", "iMac"];
-sort($products);
-print_r($products);
-  print_r($products[0]);
+// sort($products);
+// print_r($products);
+//   print_r($products[0]);
+//   echo "<br>";
+//   print_r(  $products[2]);
+
+$i=count($products);
+$n=0;
+// while( $n < $i){
+//   echo "<br>";
+//   echo $products[$n];
+//   $n++;
+
+
+// }
+
+// for ($x=0; $x<$i ;$x++){
+//   echo "<br>";
+//   echo $products[$x];
+
+// }
+
+// do{
+  
+//   echo $products[$n];
+//   $n++;
+// }while ($n < $i);
+
+
+foreach($products as $product){
   echo "<br>";
-  print_r(  $products[2]);
+  echo $product;
+}
+
+
